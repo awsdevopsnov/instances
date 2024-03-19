2006d2d73979d34f201b186a4e500fce1d18d854
awsdevopsnov <manimegalaimecse@gmail.com>
2024-03-19 13:09:48 +0530

    sixth commit

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.41.0"
+      version = "5.41.0"
     }
   }
 }
 
 provider "aws" {
   region  = "us-east-1"
-  version = "5.0"
+  version = "3.41.0"
 }
 
 resource "aws_instance" "Ec2_instance" {
