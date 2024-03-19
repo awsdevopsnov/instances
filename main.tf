ae1e91152c86aa5f065a38b058e7f71dca9f0773
awsdevopsnov <manimegalaimecse@gmail.com>
2024-03-19 12:34:55 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 5.6"
+}
+
+resource "aws_instance" "Ec2_instance" {
+    ami = "ami-0d7a109bf30624c99"
+    instance_type = "t2.micro"
+}
