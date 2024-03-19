0e6c48d04e8f39f6de3c1a9be3bd50803c252dfe
awsdevopsnov <manimegalaimecse@gmail.com>
2024-03-19 13:13:48 +0530

    eight commit

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.65.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  version = "3.41.0"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "Ec2_instance" {
-    ami = "ami-0d7a109bf30624c99"
-    instance_type = "t2.micro"
+  ami           = "ami-0d7a109bf30624c99"
+  instance_type = "t2.micro"
 }
 
-# comments
